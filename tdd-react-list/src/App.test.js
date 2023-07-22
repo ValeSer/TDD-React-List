3c4d102b7c42ff2cec3b3eb428ea49e47ad41815
Valeria Serci <balerty84@gmail.com>
2023-07-22 19:42:40 +0100

    test failing for a rendering a list of items

--- tdd-react-list/src/App.test.js
+++ tdd-react-list/src/App.test.js
@@ -3,6 +3,8 @@ import App from './App';
 
 describe('App', () => {
   it('', () => {
-    
+    render(<App />);
+    const listItems = screen.getAllByRole("listitem");
+    expect(listItems).toHaveLength(1)
   })
 });
