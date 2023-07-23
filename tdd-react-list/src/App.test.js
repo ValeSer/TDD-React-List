b9b5e9f397212466abecaf0071604b54123541ec
Valeria Serci <balerty84@gmail.com>
2023-07-23 23:35:19 +0100

    refactor: add title to the test

--- tdd-react-list/src/App.test.js
+++ tdd-react-list/src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  it('', () => {
+  it('renders one item', () => {
     render(<App />);
     const listItems = screen.getAllByRole("listitem");
     expect(listItems).toHaveLength(1)
