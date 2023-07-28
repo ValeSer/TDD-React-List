046db51b96d058008c39f045027686acd8247dc7
Valeria Serci <balerty84@gmail.com>
2023-07-28 22:08:48 +0100

    implement test for username

--- tdd-react-list/src/components/Login.test.js
+++ tdd-react-list/src/components/Login.test.js
@@ -1,7 +1,8 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { Login } from './Login';
 
 it('username input should be rendered', () => {
   render(<Login/>);
-
+  const userInputEl = screen.getByPlaceholderText(/username/i);
+  expect(userInputEl).toBeInTheDocument();
 });
