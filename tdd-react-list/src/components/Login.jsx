1a9847f32c02a88ac02882c31263b32230dc8720
Valeria Serci <balerty84@gmail.com>
2023-07-30 22:44:13 +0100

    remove value for username to make the test pass

--- tdd-react-list/src/components/Login.jsx
+++ tdd-react-list/src/components/Login.jsx
@@ -2,7 +2,7 @@ export const Login = () => {
   return (
     <div className="container">
       <form>
-        <input type="text" placeholder="username" value="username"/>
+        <input type="text" placeholder="username" />
         <input type="password" placeholder="password" value="password"/>
         <button>Login </button>
       </form>
