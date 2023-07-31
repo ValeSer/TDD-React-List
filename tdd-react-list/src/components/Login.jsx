c25fee3b0d3805e8e946a6254c241dbc330ee574
Valeria Serci <balerty84@gmail.com>
2023-07-31 16:05:48 +0100

    failing test for username and password onChange

--- tdd-react-list/src/components/Login.jsx
+++ tdd-react-list/src/components/Login.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 export const Login = () => {
   const [error, setError] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <div className="container">
       <form>
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
+        <input type="text" placeholder="username" value={username}/>
+        <input type="password" placeholder="password" value={password}/>
         <button disabled={true}>Login </button>
         <span 
           data-testid="error"
--- tdd-react-list/src/components/Login.test.js
+++ tdd-react-list/src/components/Login.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { Login } from './Login';
 
 it('username input should be rendered', () => {
@@ -41,4 +41,22 @@ it('error message should not be visible', () => {
   render(<Login />);
   const errorEl = screen.getByTestId('error');
   expect(errorEl).not.toBeVisible()
+});
+
+it('username input should change', () => {
+  render(<Login/>);
+  const usernameInputEl = screen.getByPlaceholderText(/username/i);
+  const testValue = 'test';
+
+  fireEvent.change(usernameInputEl, { target: { value: testValue } });
+  expect(usernameInputEl.value).toBe(testValue);
+});
+
+it('password input should change', () => {
+  render(<Login/>);
+  const passwordInputEl = screen.getByPlaceholderText(/password/i);
+  const testValue = 'test';
+
+  fireEvent.change(passwordInputEl, { target: { value: testValue } });
+  expect(passwordInputEl.value).toBe(testValue);
 });
