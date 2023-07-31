4c11dbe3c022b3d75a839b13bfb65910ca9e86e4
Valeria Serci <balerty84@gmail.com>
2023-07-31 11:43:11 +0100

    fix true with curly braces and defined the style inside span tag. Test is now passing

--- tdd-react-list/src/components/Login.jsx
+++ tdd-react-list/src/components/Login.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 export const Login = () => {
+  const [error, setError] = useState(false);
   return (
     <div className="container">
       <form>
         <input type="text" placeholder="username" />
         <input type="password" placeholder="password" />
-        <button disabled="true">Login </button>
-        <span data-testid="error">Something went wrong</span>
+        <button disabled={true}>Login </button>
+        <span 
+          data-testid="error"
+          style={{visibility: error ? "visible" : "hidden"}}
+        >
+          Something went wrong
+        </span>
       </form>
     </div>
   );
