0b70fa2a62d672c80488a8107e69de11ab9adc3a
Valeria Serci <balerty84@gmail.com>
2023-07-31 16:34:40 +0100

    failing test for button should not be disabled when input exists

--- tdd-react-list/src/components/Login.test.js
+++ tdd-react-list/src/components/Login.test.js
@@ -60,3 +60,17 @@ it('password input should change', () => {
   fireEvent.change(passwordInputEl, { target: { value: testValue } });
   expect(passwordInputEl.value).toBe(testValue);
 });
+
+it('button should not be disabled when input exists', () => {
+  render(<Login/>);
+  const buttonInputEl = screen.getByRole("button");
+  const usernameInputEl = screen.getByPlaceholderText(/username/i);
+  const passwordInputEl = screen.getByPlaceholderText(/password/i);
+
+  const testValue = 'test';
+
+  fireEvent.change(usernameInputEl, { target: { value: testValue } });
+  fireEvent.change(passwordInputEl, { target: { value: testValue } });
+
+  expect(buttonInputEl).not.toBeDisabled();
+});
