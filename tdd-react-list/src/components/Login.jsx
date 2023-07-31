d5171a778f219d559bdce4d34500ba4875ae60a6
Valeria Serci <balerty84@gmail.com>
2023-07-31 16:52:10 +0100

    implement clickHandler for login button

--- tdd-react-list/src/components/Login.jsx
+++ tdd-react-list/src/components/Login.jsx
@@ -3,8 +3,23 @@ export const Login = () => {
   const [error, setError] = useState(false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [user, setUser] = useState({});
+
+  const clickHandler = async (e) => {
+    e.preventDefault();
+    try{
+      const {data} = axios.get(
+        "https://jsonplaceholder.typicode.com/users/1"
+        );
+        setUser(data);
+    }catch{
+      setError(true);
+    }
+  };
+
   return (
     <div className="container">
+      <span className="user">{user.name}</span>
       <form>
         <input 
           type="text" 
@@ -18,7 +33,12 @@ export const Login = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button disabled={!username || !password}>Login </button>
+        <button 
+          disabled={!username || !password}
+          onClick={clickHandler}
+        >
+          Login 
+        </button>
         <span 
           data-testid="error"
           style={{visibility: error ? "visible" : "hidden"}}
