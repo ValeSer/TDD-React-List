eb495f32725c9c724044b30bfbdbdfba2d3b1989
Valeria Serci <balerty84@gmail.com>
2023-08-01 14:43:13 +0100

    failing test for loading should not be visible after fetching

--- tdd-react-list/src/components/Login.test.js
+++ tdd-react-list/src/components/Login.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { Login } from './Login';
 
 jest.mock('axios', () => ({
@@ -104,4 +104,19 @@ it('loading should be rendered when click', () => {
   fireEvent.click(buttonEl);
 
   expect(buttonEl).toHaveTextContent(/please wait/i);
+});
+
+it('loading should not be visible after fetching', async () => {
+  render(<Login/>);
+  const buttonEl = screen.getByRole("button");
+  const usernameInputEl = screen.getByPlaceholderText(/username/i);
+  const passwordInputEl = screen.getByPlaceholderText(/password/i);
+
+  const testValue = 'test';
+
+  fireEvent.change(usernameInputEl, { target: { value: testValue } });
+  fireEvent.change(passwordInputEl, { target: { value: testValue } });
+  fireEvent.click(buttonEl);
+
+  await waitFor(() => expect(buttonEl).not.toHaveTextContent(/please wait/i));
 });
