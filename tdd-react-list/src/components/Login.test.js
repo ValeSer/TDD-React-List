d6799805f0a4deec2033ff74826440364da120ef
Valeria Serci <balerty84@gmail.com>
2023-08-01 16:26:57 +0100

    failing test for user should be visible after fetching

--- tdd-react-list/src/components/Login.test.js
+++ tdd-react-list/src/components/Login.test.js
@@ -1,6 +1,7 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { Login } from './Login';
 import { act } from 'react-dom/test-utils'
+import { toBeInTheDocument } from '@testing-library/jest-dom/matchers';
 
 jest.mock('axios', () => ({
   __esModule:true,
@@ -120,4 +121,20 @@ it('loading should not be visible after fetching', async () => {
   fireEvent.click(buttonEl);
 
   await waitFor(() => expect(buttonEl).not.toHaveTextContent(/please wait/i));
+});
+
+it('user should be visible after fetching', async () => {
+  render(<Login/>);
+  const buttonEl = screen.getByRole("button");
+  const usernameInputEl = screen.getByPlaceholderText(/username/i);
+  const passwordInputEl = screen.getByPlaceholderText(/password/i);
+
+  const testValue = 'test';
+
+  fireEvent.change(usernameInputEl, { target: { value: testValue } });
+  fireEvent.change(passwordInputEl, { target: { value: testValue } });
+  fireEvent.click(buttonEl);
+
+  const userItem = await screen.findByText("John")
+  expect(userItem).toBeInTheDocument();
 });
